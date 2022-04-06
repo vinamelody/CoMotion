08b9305295fbf631b51db95cdf0c6449bd3c41a8
Vina Melody <vina_melody@yahoo.com>
2022-04-06 21:42:31 +0800

    Initial Commit

---
+++ CoMotion/CoMotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoMotionApp.swift
+//  CoMotion
+//
+//  Created by Vina Melody on 6/4/22.
+//
+
+import SwiftUI
+
+@main
+struct CoMotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoMotion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoMotion
+//
+//  Created by Vina Melody on 6/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
